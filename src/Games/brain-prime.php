76307345bea5a7c2ec10ad8c98acf4560855143a
ruzil <muftakhovruzil@yandex.ru>
2025-02-25 20:56:26 +0500

    final

--- src/Games/brain-prime.php
+++ src/Games/brain-prime.php
@@ -17,7 +17,7 @@ const RULES = 'Answer "yes" if given number is prime. Otherwise answer "no".';  
 function isPrime(int $number)
 {
     $array = array(2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97);
-    return in_array($number, $array ,false);
+    return in_array($number, $array, false);
 }
 
 function genQuestionAndAnswer(): array
